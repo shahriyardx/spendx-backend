2961dfaa92b1f86bb3b60b78e0aef5ed084818cf
Shahriyar <mdshahriyaralam552@gmail.com>
2026-05-02 14:13:23 +0600

    use sqlite db

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,9 @@
 import { expo } from "@better-auth/expo";
 import { betterAuth } from "better-auth";
+import Database from "better-sqlite3";
 
 export const auth = betterAuth({
+  database: new Database("database.sqlite"),
   baseURL: process.env.BETTER_AUTH_URL,
   secret: process.env.BETTER_AUTH_SECRET,
   plugins: [expo()],
